53b0e1d4a966f953c71e0bd5ea3b8749846032c0
Luis Morquecho <luisiyi1997@gmail.com>
2021-05-09 22:07:33 -0700

    Aplicación conectada con Firebase e inicio de sesión.

---
+++ app/src/main/java/com/example/digimind/ContrasenaActivity.kt
@@ -0,0 +1,46 @@
+package com.example.digimind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class ContrasenaActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contrasena)
+
+        auth = Firebase.auth
+
+        val btn_restablecer: Button = findViewById(R.id.btn_restablecer)
+
+        btn_restablecer.setOnClickListener{
+            val et_correo: EditText = findViewById(R.id.et_correo_cont)
+
+            var correo: String = et_correo.text.toString()
+
+            if(!correo.isNullOrBlank()){
+                auth.sendPasswordResetEmail(correo)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful){
+                            Toast.makeText(this, "Se envio un correo a $correo",
+                                Toast.LENGTH_SHORT).show()
+                        }else{
+                            Toast.makeText(this, "Error al enviar correo",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this,"Ingresar correo",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/digimind/LoginActivity.kt
@@ -0,0 +1,80 @@
+package com.example.digimind
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val btn_registrarse: Button = findViewById(R.id.btn_registrarse)
+        val btn_contra: TextView = findViewById(R.id.tv_olvidasteContra)
+        val btn_ingresar: Button = findViewById(R.id.btn_ingresar)
+
+        auth = Firebase.auth
+
+        btn_registrarse.setOnClickListener{
+            val intent: Intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_contra.setOnClickListener{
+            val intent: Intent = Intent(this, ContrasenaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_ingresar.setOnClickListener{
+            valida_ingreso()
+        }
+    }
+
+    private fun valida_ingreso(){
+        val et_correo: EditText = findViewById(R.id.et_correo)
+        val et_contra: EditText = findViewById(R.id.et_contra)
+
+        var correo: String = et_correo.text.toString()
+        var contra: String = et_contra.text.toString()
+
+        if(!correo.isNullOrBlank() && !contra.isNullOrBlank()){
+            ingresaFirebase(correo, contra)
+        }else{
+            Toast.makeText(this, "Ingresar datos",
+                Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun ingresaFirebase(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+
+                    val user = auth.currentUser
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+    }
+}
